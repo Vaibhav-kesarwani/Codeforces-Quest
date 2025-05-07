487f16288fb063e2f8be54cba8938f473cc90c22
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-05-07 19:08:40 +0530

    Make the browser api to call the local storage to store the UI changes and custom theme changes throughout the browser

--- public/assets/scripts/content.js
+++ public/assets/scripts/content.js
@@ -1,2 +1,22 @@
 const isFirefox = typeof browser !== 'undefined';
 const browserAPI = isFirefox ? browser : chrome;
+
+browserAPI.storage.local.get(["changeUI", "theme", "themeCustomSettings", "defaultThemeSettings"]).then((result) => {
+    if (result.changeUI === "true") {
+        document.addEventListener("DOMContentLoaded", () => {
+            changeLoginPageUI();
+            changeProblemSetPageUI();
+        });
+    }
+
+    if (result.theme === "dark") {
+        injectDarkModeCSS();
+        sortToggleImgInvert();
+
+        if (result.themeCustomSettings) {
+            applyCustomThemeSettings(result.themeCustomSettings);
+        } else if (result.defaultThemeSettings) {
+            applyCustomThemeSettings(result.defaultThemeSettings);
+        }
+    }
+});
