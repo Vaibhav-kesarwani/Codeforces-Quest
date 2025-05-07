61418f03867de7584b2a6f2da960a0920a38b826
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-05-07 19:39:22 +0530

    Collecting the problem and the testcases from the screen when the problem appeared

--- public/assets/scripts/darkModeFunctions/darkMode.js
+++ public/assets/scripts/darkModeFunctions/darkMode.js
@@ -2,3 +2,7 @@ if (typeof isFirefox === 'undefined') {
     let isFirefox = typeof browser !== 'undefined';
     let browserAPI = isFirefox ? browser : chrome;
 }
+
+const currentURL = window.location.href;
+let styleElement;
+let customStyleElement;
