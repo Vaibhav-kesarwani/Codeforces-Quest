d1dffe9592b15176f46e678938e765870a974d0f
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-11-02 20:10:08 +0530

    Refactor the constants for the extension

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,10 +1,10 @@
 import { EditorSettingsTypes, ShortcutSettings } from "../types/types";
 
-export const STORAGE_LIMIT_BYTES = 7 * 1024 * 1024; // local storage limit in bytes, 7MB
-export const SINGLE_CODE_LIMIT_BYTES = 0.5 * 1024 * 1024; // single code limit in bytes, 0.5MB
-export const MAX_PROBLEM_IO_SIZE = 15; // max number problem IO size
-export const MAX_TEST_CASES = 5; // max number of test cases
-export const EXECUTE_CODE_LIMIT = 3 * 1000; // max number of minutes to execute code
+export const STORAGE_LIMIT_BYTES = 7 * 1024 * 1024; 
+export const SINGLE_CODE_LIMIT_BYTES = 0.5 * 1024 * 1024;
+export const MAX_PROBLEM_IO_SIZE = 15; 
+export const MAX_TEST_CASES = 5; 
+export const EXECUTE_CODE_LIMIT = 3 * 1000;
 
 export const accessRestrictionMessage = `/* 
 Code Editor Access:
